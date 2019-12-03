afa098222f59bf3811014445377b086e242a0710
250mon <lambki@naver.com>
2019-12-03 15:49:18 +0900

    28 Browsing the web and WebView halfway

--- app/src/main/java/com/sjy/photogallery/GalleryItem.java
+++ app/src/main/java/com/sjy/photogallery/GalleryItem.java
@@ -1,11 +1,14 @@
 package com.sjy.photogallery;
 
+import android.net.Uri;
+
 import androidx.annotation.NonNull;
 
 public class GalleryItem {
     private String mCaption;
     private String mId;
     private String mUrl;
+    private String mOwner;
 
     @NonNull
     @Override
@@ -29,11 +32,27 @@ public class GalleryItem {
         mId = id;
     }
 
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
     public String getUrl() {
         return mUrl;
     }
 
     public void setUrl(String url) {
         mUrl = url;
+    }
+
+    public Uri getPhotoPageUri() {
+        return Uri.parse("http://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
     }
 }
